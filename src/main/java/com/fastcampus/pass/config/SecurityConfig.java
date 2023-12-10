c5b69627dd69d089a39d94f48c602b4a7f3c0657
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-12-10 16:15:39 +0900

    패키지 보여주는 페이지 생성
    로그인 하지 않은 유저도 볼 수 있도록 설정.
    생성된 모든 패키지 보여주도록 설정.
    HTML페이지는 이용권 페이지 수정해서 만들었습니다.

--- src/main/java/com/fastcampus/pass/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/pass/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .mvcMatchers(HttpMethod.GET, "/packaze").permitAll()
                         .mvcMatchers(HttpMethod.POST, "/**").hasAnyRole(rolesAboveManager)
                         .mvcMatchers(HttpMethod.DELETE, "/**").hasAnyRole(rolesAboveManager)
                         .anyRequest().authenticated())
--- src/main/java/com/fastcampus/pass/controller/pass/PassViewController.java
+++ src/main/java/com/fastcampus/pass/controller/pass/PassViewController.java
@@ -1,27 +1,22 @@
 package com.fastcampus.pass.controller.pass;
 
 import com.fastcampus.pass.repository.pass.PassDto;
-import com.fastcampus.pass.repository.user.UserDto;
 import com.fastcampus.pass.repository.user.security.PassPrincipal;
 import com.fastcampus.pass.service.pass.PassService;
-import com.fastcampus.pass.service.user.UserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/passes")
 public class PassViewController {
     private final PassService passService;
 
-    public PassViewController(UserService userService, PassService passService) {
+    public PassViewController(PassService passService) {
         this.passService = passService;
     }
 
