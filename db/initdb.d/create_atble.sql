12bd4b0af51ab69cfe70f1a1ca9429c9ba5146f2
Sungin Lee <100878663+sungin95@users.noreply.github.com>
2023-12-13 11:32:34 +0900

    Update create_atble.sql 주석 주성

--- db/initdb.d/create_atble.sql
+++ db/initdb.d/create_atble.sql
@@ -23,7 +23,7 @@ CREATE TABLE `pass`
     `pass_seq`    int         NOT NULL AUTO_INCREMENT COMMENT '이용권 순번',
     `user_id`     varchar(20) NOT NULL COMMENT '사용자 ID',
     `gym_period`  int                  DEFAULT NULL COMMENT '잔여 이용일 수',
-    `count_pt`    int                  DEFAULT NULL COMMENT '잔여 이용권일 수',
+    `count_pt`    int                  DEFAULT NULL COMMENT '잔여 PT 이용권 수',
     `status`      varchar(10) NOT NULL COMMENT '회원 상태 표시',
     `created_at`  timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 일시',
     `created_by`  varchar(20)          DEFAULT NULL COMMENT '최초 생성자',
