5f17bc3574c124d8165a8f6ef9c65503935596a2
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-12-15 09:31:40 +0900

    매일 밤12시에 하루씩 차감되는 Batch작성
    Batch를 사용하기 위해 BatchConfig 작성.
    
    Tastlet을 사용하여 구현.
    이유:  헬쓰장을 다니는 유저 최대 수는 500명이라고 예상. 처리해야 하는 규모가 적은편. 또한 오류가 났을때 누구는 차감되고 누구는 차감이 안되는 상황을 막기위해.
    
    런처를 매일 밤 12시에 돌아가도록 설정.
    
    findByStatus는 특정 상태의 유저를 찾기위해 제작. 여기서는 Activation 상태의 유저를 찾기 위해 사용.

--- src/main/java/com/fastcampus/pass/PassWebApplication.java
+++ src/main/java/com/fastcampus/pass/PassWebApplication.java
@@ -2,7 +2,9 @@ package com.fastcampus.pass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class PassWebApplication {
 
